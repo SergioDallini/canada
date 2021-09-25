3fef708c5ce068976a512c9092bde863c60dc596
Sergio <shossssser@gmail.com>
2021-09-26 00:33:55 +0300

    Commit1

---
+++ about.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+    <title>My Site</title>
+</head>
+<body>
+<?php require_once("blocks/header.php")?>
+
+<form action="check.php" method="post">
+
+
+</form>
+
+<?php require_once("blocks/footer.php")?>
+</body>
+</html>
